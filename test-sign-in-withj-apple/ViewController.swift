8410a9d92b1dcdee06fa30238f9d4fa945c9fcb0
miyado <hmiyado@gmail.com>
2020-06-13 14:05:39 +0900

    Initial Commit

---
+++ test-sign-in-withj-apple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test-sign-in-withj-apple
+//
+//  Created by 吉田拓真 on 2020/06/13.
+//  Copyright © 2020 hmiyado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
